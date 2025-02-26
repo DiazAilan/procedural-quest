8e6791428b621ae603b77168b23cd7ca7e4d35fe
DiazAilan <diaz.ailan.gonzalo@gmail.com>
2025-02-26 23:57:52 +0100

    [WIP] feature: add combat state

---
+++ src/app/store/combat.actions.ts
@@ -0,0 +1,17 @@
+export enum AttackType {
+  Melee = 'Melee',
+  Ranged = 'Ranged',
+  Magic = 'Magic'
+}
+
+export interface AttackAction {
+  source: string;
+  target: string;
+  attackType: AttackType;
+}
+
+export class Attack {
+  static readonly type = '[Combat] Attack';
+
+  constructor(public payload: AttackAction) {}
+}
